4e40132866096fe6716e1f8df1cb6917610ce9c6
AZIZXlaouiti <azizrocke@gmail.com>
2021-08-10 00:05:39 -0400

    completed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index{|e| puts "#{index+1}. #{e}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map.with_index{|e,index| e= e.gsub(/\S+/, &:capitalize)+"!"}
+ end
+
+ def long_planeteer_calls(short_words)
+  short_words.find{|e|  e.length >4}.length>0 ? true : false
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    puts arr.find{|e| cheese_types.any?(e)}
+
 end
